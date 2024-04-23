cb5a6ae9c08625550bad8753e468236d9f9f98e7
Ada Phillips <ragwafire99@gmail.com>
2024-04-23 17:17:36 -0400

    Update mock serial script for new config (#13)

--- test/mockSerial.py
+++ test/mockSerial.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-import os, pty, serial, time
+import os, pty, time
 
-master, slave = pty.openpty()
-s_name = os.ttyname(slave)
-m_name = os.ttyname(master)
+primary, secondary = pty.openpty()
+s_name = os.ttyname(secondary)
+m_name = os.ttyname(primary)
+
+COMP_RESPOND_COMMANDS = [
+    "g0","g1","move_plate","home_axis","dwell"
+]
+COMP_RESPONSE = bytes('Z_move_comp\r\n', 'utf-8')
+
+STATUS_COMMAND = "status"
+STATUS_DISCONNECT = bytes('Klipper state: Disconnect\r\n', 'utf-8')
+STATUS_READY = bytes('Klipper state: Ready\r\n', 'utf-8')
+
 print(f"Virtual Serial Port: {s_name}")
 
 status_counter = 0
@@ -11,16 +21,16 @@ status_counter = 0
 while True:
     # To read from the device
     time.sleep(1)
-    commands = os.read(master,1000).decode('utf-8')
+    serial_input = os.read(primary,1000).decode('utf-8')
 
-    if commands:
-        print(f"Received: {commands}")
+    if serial_input:
+        print(f"Received: {serial_input}")
 
-    if "g0" in commands.lower() or "g1" in commands.lower():
-        os.write(master, bytes('Z_move_comp\r\n', 'utf-8'))
-    elif "status" in commands.lower():
+    if any(command in serial_input.lower() for command in COMP_RESPOND_COMMANDS):
+        os.write(primary,COMP_RESPONSE)
+    elif STATUS_COMMAND in serial_input.lower():
         if status_counter<10:
             status_counter += 1
-            os.write(master, bytes('Klipper state: Disconnect\r\n', 'utf-8'))
+            os.write(primary, STATUS_DISCONNECT)
         else:
-            os.write(master, bytes('Klipper state: Ready\r\n', 'utf-8'))
+            os.write(primary, STATUS_READY)
